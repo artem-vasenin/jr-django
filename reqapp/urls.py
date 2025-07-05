093c726d18595976d181d40cbdc36fb88d98cd1a
Rusich <vasenin@verqor.com>
2025-07-05 22:55:47 +0300

    added bio form

--- reqapp/urls.py
+++ reqapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, bio
 
 app_name = 'reqapp'
 
 urlpatterns = [
     path('', index, name='reqapp_home'),
+    path('bio', bio, name='reqapp_bio'),
 ]
--- reqapp/views.py
+++ reqapp/views.py
@@ -1,8 +1,12 @@
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     name = request.GET.get('name', 'Unknown')
     surname = request.GET.get('surname', 'Unknown')
     context = {'full_name': f'{name} {surname}'}
     return render(request, 'reqapp/index.html', context=context)
+
+def bio(request: HttpRequest) -> HttpResponse:
+    return render(request, 'reqapp/bio.html')
