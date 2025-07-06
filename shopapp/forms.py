414302d3a3b82f529406b20e3e77fb15cf0bdc11
Rusich <vasenin@verqor.com>
2025-07-06 23:58:18 +0300

    added bio form validators

--- shopapp/forms.py
+++ shopapp/forms.py
@@ -1,7 +1,17 @@
+from django.core import validators
+
 from django import forms
 
 class ProductForm(forms.Form):
     name = forms.CharField(label='Name', max_length=100)
     price = forms.IntegerField(label='Price', min_value=0, max_value=999999)
-    description = forms.CharField(label='Description', widget=forms.Textarea, required=False)
+    description = forms.CharField(
+        label='Description',
+        widget=forms.Textarea(attrs={'rows': 5}),
+        required=False,
+        validators=[validators.RegexValidator(
+            regex=r'great',
+            message='Field must contain field "great"'
+        )]
+    )
     discount = forms.IntegerField(label='discount', min_value=0, max_value=100)
