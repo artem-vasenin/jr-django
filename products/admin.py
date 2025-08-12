f4f06972b62a2d96251f0a38c24a5ecd2301f1fb
Rusich <vasenin@verqor.com>
2025-08-12 22:42:29 +0300

    added product models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Product, ProductCategory, ProductOption
 
-# Register your models here.
+class ProductOptionInline(admin.TabularInline):
+    model = ProductOption
+    extra = 1
+    min_num = 0
+    verbose_name = 'Опция товара'
+    verbose_name_plural = 'Опции товара'
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'in_store', 'updated_at')
+    list_filter = ('category', 'unit')
+    search_fields = ('name', 'short_desc', 'description')
+    inlines = [ProductOptionInline]
+    filter_horizontal = ('category',)
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+
+
+@admin.register(ProductOption)
+class ProductOptionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'value', 'product')
+    search_fields = ('name', 'value')
+    list_filter = ('product',)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=600, unique=True, verbose_name='Название категории')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    UNIT_CHOICES = [
+        ('шт', 'штуки'),
+        ('л', 'литры'),
+        ('мл', 'миллилитры'),
+        ('т', 'тонны'),
+        ('кг', 'килограммы'),
+        ('г', 'граммы'),
+        ('м', 'метры'),
+        ('см', 'сантиметры'),
+    ]
+
+    name = models.CharField(max_length=600, unique=True, verbose_name='Название товара')
+    price = models.PositiveIntegerField(verbose_name='Цена')
+    short_desc = models.CharField(max_length=600, blank=True, null=True, verbose_name='Краткое описание')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    unit = models.CharField(max_length=20, choices=UNIT_CHOICES, default='шт', verbose_name='Единица измерения')
+    in_store = models.PositiveIntegerField(verbose_name='Количество на складе')
+    img = models.ImageField(upload_to='products/', null=True, blank=True, verbose_name='Изображение')
+    category = models.ManyToManyField(ProductCategory, blank=True, verbose_name='Категории')
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class ProductOption(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название опции',)
+    value = models.CharField(max_length=400, verbose_name='Значение опции',)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='options', verbose_name='Товар')
+
+    def __str__(self):
+        return self.name
