c55a04040bcfb8a0ab3e869e789b19bace6e8be5
Rusich <vasenin@verqor.com>
2025-10-05 20:32:23 +0300

    added category templates

--- management/urls.py
+++ management/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from .views import ManagementView, ManagementProductView, ManagementProductsView, ManagementAddProductView
+from .views import (
+    ManagementView, ManagementProductView, ManagementProductsView, ManagementAddProductView,
+    ManagementCategoriesView, ManagementCategoryView, ManagementAddCategoryView,
+)
 
 
 urlpatterns = [
     path('', ManagementView.as_view(), name='management'),
     path('products/', ManagementProductsView.as_view(), name='management-products'),
     path('product/<int:pk>/', ManagementProductView.as_view(), name='management-product'),
-    path('add/', ManagementAddProductView.as_view(), name='management-add'),
+    path('product/add/', ManagementAddProductView.as_view(), name='management-add-product'),
+    path('categories/', ManagementCategoriesView.as_view(), name='management-categories'),
+    path('category/<int:pk>/', ManagementCategoryView.as_view(), name='management-category'),
+    path('category/add/', ManagementAddCategoryView.as_view(), name='management-add-category'),
 ]
--- management/views.py
+++ management/views.py
@@ -20,3 +20,15 @@ class ManagementProductView(SuperuserRequiredMixin, View):
 class ManagementAddProductView(SuperuserRequiredMixin, View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, 'management/add-product.html')
+
+class ManagementCategoriesView(SuperuserRequiredMixin, View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'management/categories.html')
+
+class ManagementCategoryView(SuperuserRequiredMixin, View):
+    def get(self, request: HttpRequest, pk) -> HttpResponse:
+        return render(request, 'management/category.html', {pk: pk})
+
+class ManagementAddCategoryView(SuperuserRequiredMixin, View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'management/add-category.html')
